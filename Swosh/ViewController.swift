dd3c5467b2cef8041a8a4d75916ff68f0bf0251a
Noah <noaheaton@noahs-macbook-pro.local>
2018-08-12 11:33:32 -0400

    Initial Commit

---
+++ Swosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swosh
+//
+//  Created by Noah Eaton on 8/12/18.
+//  Copyright © 2018 Noah Eaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
