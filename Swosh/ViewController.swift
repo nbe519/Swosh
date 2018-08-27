d5e8d7795487322b724958b4338defe476e5deaf
Noah Eaton <noaheaton@Noahs-MBP.T-mobile.com>
2018-08-27 11:01:29 -0400

    Add constraints

--- Swosh/ViewController.swift
+++ Swosh/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         
     }
+    
 
 
 }
